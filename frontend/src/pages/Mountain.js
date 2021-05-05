8809621b282e509253953ac6e6eed8bddb9ff42c
Jaimee <jmcl168@aucklanduni.ac.nz>
2021-05-05 20:16:05 +1200

    Testing API functionality on frontend pages

--- frontend/src/pages/Mountain.js
+++ frontend/src/pages/Mountain.js
@@ -1,15 +1,25 @@
 import {Link, useHistory} from "react-router-dom";
+import { AppContext } from '../AppContext';
+import { useContext } from 'react';
 
 function Mountain(){
+    const { mountains, mountainsLoading } = useContext(AppContext);
+//only showing turoa data, page not linked to individuals yet
+    if (mountainsLoading) {
+        return null;
+    } else{
 
     return(
         <div> 
-            MOUNTAIN
-            
+            <div>{mountains[0].Name}</div>
+            <div>{mountains[0].Description}</div>
+            <div> X coord :{mountains[0].location.Xcoord} </div>
+            <div> Y coord :{mountains[0].location.Ycoord} </div>
             <Link to = '/home'>
                 <button> Back  </button>
             </Link>
         </div>
     );
+    }
 }
 export default Mountain;
