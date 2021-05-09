ae9dac3152934e4e14b4a4f5f0800aeb2d92907e
Jaimee McLaughlan <61758619+Jmcl168@users.noreply.github.com>
2021-05-09 23:58:59 +1200

    no message (#41)

--- frontend/src/components/WeatherDash/TodayWeather.js
+++ frontend/src/components/WeatherDash/TodayWeather.js
@@ -48,7 +48,7 @@ export default function TodayWeather({latProp, longProp}){
        <div>
 
                <div className="weatherdash-rightnow-icon-mountainpage">
-                   <img src={"http://openweathermap.org/img/w/" + weatherSymbol + ".png"} alt="sun" height='200px' width='200px'></img>
+                   <img src={"http://openweathermap.org/img/w/" + weatherSymbol + ".png"} alt="sun" height='150px' width='150px'></img>
                    
                </div>
                <div className="weatherdash-rightnow-header">
